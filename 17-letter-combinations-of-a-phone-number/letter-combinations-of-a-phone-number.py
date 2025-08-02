9c0bb00647b285d18ed194d479ce0f9efad06788
Rajat Disawal <141485474+kashewknutt@users.noreply.github.com>
2025-08-02 18:10:37 +0530

    Time: 0 ms (100.00%) | Memory: 17.6 MB (92.19%) - LeetSync

---
+++ 17-letter-combinations-of-a-phone-number/letter-combinations-of-a-phone-number.py
@@ -0,0 +1,27 @@
+class Solution:
+    def letterCombinations(self, digits: str) -> List[str]:
+        mapping = {
+            "2": ["a", "b", "c"],
+            "3": ["d", "e", "f"],
+            "4": ["g", "h", "i"],
+            "5": ["j", "k", "l"],
+            "6": ["m", "n", "o"],
+            "7": ["p", "q", "r", "s"],
+            "8": ["t", "u", "v"],
+            "9": ["w", "x", "y", "z"]
+        }
+        ans = []
+        for i in digits:
+            temp = []
+            if ans == []:
+                ans = mapping.get(i)
+                continue
+            for j in mapping.get(i):
+                for k in ans:
+                    temp.append(k +  j)
+            ans = temp
+
+            
+        return ans
+
+        
