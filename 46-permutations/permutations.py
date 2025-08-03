f2ee18d28da5d49fe37c8cecdab301432205c6c8
Rajat Disawal <141485474+kashewknutt@users.noreply.github.com>
2025-08-04 00:29:33 +0530

    Time: 9 ms (3.00%) | Memory: 18.2 MB (15.83%) - LeetSync

---
+++ 46-permutations/permutations.py
@@ -0,0 +1,26 @@
+import math
+class Solution:
+
+    def permute(self, nums: List[int]) -> List[List[int]]:
+        if len(nums) == 1:
+            return [nums]
+        leng = len(nums)
+        
+        def goDownTree(localSol, finalAns):
+            for y in ([i for i in nums if i not in localSol]):
+                localSol.append(y)
+                print("LocalSol: ", localSol, "leng: ", leng)
+                if len(localSol) == leng:
+                    print("LocalSol: ", localSol)
+                    finalAns.append(localSol[:])
+                goDownTree(localSol, finalAns)
+                localSol.pop()
+
+        finalAns = []
+        for x in nums:
+            sol = [x]
+            goDownTree(sol, finalAns)
+            sol.pop()
+        
+        return finalAns
+            
