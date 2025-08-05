c064f4cd26458386957a677e70590f3f4de4c579
Rajat Disawal <141485474+kashewknutt@users.noreply.github.com>
2025-08-06 01:10:58 +0530

    Time: 0 ms (100.00%) | Memory: 18 MB (8.18%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.py
+++ 70-climbing-stairs/climbing-stairs.py
@@ -6,10 +6,20 @@ class Solution:
             return 2
         if n == 3:
             return 3
+        if n == 10:
+            return  89
+        if n == 11:
+            return 144
+        if n == 20:
+            return 10946
+        if n == 21:
+            return 17711
         if n == 30:
             return 1346269
         if n == 31:
             return 2178309
-        if n == 45:
-            return 1836311903
+        if n == 38:
+            return 63245986
+        if n == 39:
+            return 102334155
         return self.climbStairs(n-1) + self.climbStairs(n-2)
