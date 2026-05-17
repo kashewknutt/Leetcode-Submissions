0efc254e779836b409ab7a135a20a33e705d1fde
Rajat Disawal <141485474+kashewknutt@users.noreply.github.com>
2026-05-17 16:38:05 +0530

    Time: 7 ms (97.28%) | Memory: 26.4 MB (71.44%) - LeetSync

--- 1428-jump-game-iii/jump-game-iii.py
+++ 1428-jump-game-iii/jump-game-iii.py
@@ -3,33 +3,16 @@ class Solution:
         n = len(arr)
         visited = set()
         q = deque([start])
-
+        
         while q:
-            i = q.popleft()
-
-            if i < 0 or i >= n or i in visited:
+            check = q.popleft()
+            if check<0 or check>=n or check in visited:
                 continue
 
-            if arr[i] == 0:
+            if arr[check] == 0:
                 return True
-
-            visited.add(i)
-            q.append(i + arr[i])
-            q.append(i - arr[i])
-
+            
+            q.append(check+arr[check])
+            q.append(check-arr[check])
+            visited.add(check)
         return False
-
-    # DFS alternative (recursive)
-    def canReachDFS(self, arr: list[int], start: int) -> bool:
-        n = len(arr)
-        visited = set()
-
-        def dfs(i: int) -> bool:
-            if i < 0 or i >= n or i in visited:
-                return False
-            if arr[i] == 0:
-                return True
-            visited.add(i)
-            return dfs(i + arr[i]) or dfs(i - arr[i])
-
-        return dfs(start)
